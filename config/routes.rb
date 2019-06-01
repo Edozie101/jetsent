3d1d2ae9a9f15260ef12eb70c242bf4927cc5b65
blutooth <maximilian@live.co.uk>
2019-06-01 20:46:40 +0100

    adding a navbar and half of chats

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   devise_for :travellers
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  #
+
+  root controller: :rooms, action: :index
+  resources :room_messages
+  resources :rooms
+
   root to: "home#index"
 end
