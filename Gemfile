9ae4c62e061b611839bfa378c5e2116f6c67ecae
blutooth <maximilian@live.co.uk>
2019-06-01 22:39:50 +0100

    added websockets to the channels

--- Gemfile
+++ Gemfile
@@ -19,6 +19,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
 
+
+# redis 
+gem 'redis'
+
 # Mechanize
 gem 'mechanize'
 # Use CoffeeScript for .coffee assets and views
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
+
 end
