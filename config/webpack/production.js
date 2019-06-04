371bf33a67ed0ab44459a61ef5843f14ef58267f
blutooth <maximilian@live.co.uk>
2019-06-04 21:44:58 +0100

    sass loader

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,3 +3,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+const sass_loader = {options: {
+    data: "$safe-area-inset-left: 200px; $another-value: 100px"
+}}
+
+environment.loaders.append( "sass-loader", sass_loader
+
+)
