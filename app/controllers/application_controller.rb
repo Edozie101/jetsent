6e172c902764b48bd6df85c33febdc941f0712ce
edzye101 <edzye101@gmail.com>
2019-06-11 07:15:23 +0100

    added change for heroku also added guest false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < ActionController::Base
   # to hand off from guest_user to current_user.
 
   def create_guest_user
-    u = User.new(:name => "guest", :email => "guest_#{Time.now.to_i}#{rand(100)}@example.com")
+    u = User.new(:name => "guest", :email => "guest_#{Time.now.to_i}#{rand(100)}@example.com", :guest => true)
     u.save!(:validate => false)
     session[:guest_user_id] = u.id
     u
